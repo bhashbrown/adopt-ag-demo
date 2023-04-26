0fd72d9cbf1a8e41074eea797f8f4f9d15e603f8
b-hash <brianmhayashi@gmail.com>
2023-04-26 13:47:14 -0700

    (add) styles to home page button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,24 @@
 import styles from '@/styles/Home.module.css';
+import { Inter } from 'next/font/google';
 import Link from 'next/link';
 import Page from '@/components/page';
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <Page>
       <Link className={styles.center} href="/mapbox">
-        Adopt-Ag Map
+        <button
+          className={inter.className}
+          style={{
+            backgroundColor: 'transparent',
+            border: 'solid 2px rgba(172, 175, 176, 0.3)',
+            borderRadius: '0.3rem',
+            padding: '0.5rem 1rem',
+          }}
+        >
+          Create Map
+        </button>
       </Link>
     </Page>
   );
