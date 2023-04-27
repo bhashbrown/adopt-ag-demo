8d0db9c7588c2545a1b769c50fb7446fdb567a5f
b-hash <brianmhayashi@gmail.com>
2023-04-27 14:05:28 -0700

    (add) cuid2 package

--- src/pages/api/polygonData.ts
+++ src/pages/api/polygonData.ts
@@ -2,8 +2,10 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import fsPromises from 'fs/promises';
 import path from 'path';
 import { FeatureCollection } from 'geojson';
+import { createId } from '@paralleldrive/cuid2';
 
 type PolygonData = {
+  id: string;
   date: string; // ISO Date string
   featureCollection: FeatureCollection; // GeoJSON
 };
@@ -20,33 +22,42 @@ export default async function processPolygonData(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
+  if (req.method !== 'POST') {
+    return res
+      .status(405)
+      .json({ message: 'This API can only accept POST requests.' });
+  }
+
+  // define array outside the promise chain so the updated data
+  // can be returned to the client after writing to the JSON file
   let polygonDataArray: PolygonData[] = [];
 
-  return fsPromises
-    .readFile(dataFilePath)
-    .then((buffer) => {
-      polygonDataArray = JSON.parse(buffer.toString());
+  // define new polygon data with a unique id
+  const newPolygon: PolygonData = {
+    id: createId(),
+    ...req.body,
+  };
 
-      if (req.method === 'GET') {
-        return res.status(200).json(polygonDataArray);
-      }
-
-      if (req.method === 'POST') {
-        const newPolygon: PolygonData = req.body;
+  return (
+    fsPromises
+      .readFile(dataFilePath)
+      .then((buffer) => {
+        // fetch/parse current data from the local JSON file
+        polygonDataArray = JSON.parse(buffer.toString());
+        // add new save file to the data array
         polygonDataArray.push(newPolygon);
+        // turn data back into JSON string
         const updatedPolygonData = JSON.stringify(polygonDataArray);
+        // add the updated data to the local JSON file
         return fsPromises.writeFile(dataFilePath, updatedPolygonData);
-      }
-    })
-    .then(() => {
-      if (req.method === 'POST') {
-        return res.status(200).json(polygonDataArray);
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-      return res
-        .status(500)
-        .json({ message: 'Error storing/fetching map data' });
-    });
+      })
+      // return updated polygon array to the client
+      .then(() => res.status(200).json(polygonDataArray))
+      .catch((error) => {
+        console.error(error);
+        return res
+          .status(500)
+          .json({ message: 'Error storing polygon map data' });
+      })
+  );
 }
